ed1862aab004307de6b970eba4c7bc787c54907e
belov <107872776+user122664623@users.noreply.github.com>
2024-03-01 00:51:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,39 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+    if (value == 1) return false;
+
+    bool isPrime = true;
+
+    for (uint64_t i = 2; i < value; i++) {
+        if (value % i == 0) isPrime = false;
+        if (!isPrime) break;
+    }
+
+    return isPrime;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    uint64_t j = 0;
+
+    for (uint64_t i = 2; ; i++) {
+        if (checkPrime(i)) j++;
+        if (j == n) return i;
+    }
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    for (uint64_t i = value + 1; ; i++) {
+        if (checkPrime(i)) return i;
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+    uint64_t sum = 0;
+
+    for (uint64_t i = 2; i < hbound; i++) {
+        if (checkPrime(i)) sum += i;
+    }
+
+    return sum;
 }
